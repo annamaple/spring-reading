fa6cbe1dead255da1fdfebf8325696a1d2861a84
anna <xionglei1614@163.com>
2025-02-26 16:07:29 +0800

    learn(spring-resources): 调试FileSystemResourceDemo

--- spring-resources/spring-resource/src/main/java/com/xcs/spring/FileSystemResourceDemo.java
+++ spring-resources/spring-resource/src/main/java/com/xcs/spring/FileSystemResourceDemo.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 public class FileSystemResourceDemo {
     public static void main(String[] args) throws Exception {
         // 请替换你自己的目录
-        String path = "D:\\idea-work-space-xcs\\spring-reading\\spring-resources\\spring-resource\\myfile.txt";
+        String path = "/Users/anna/IdeaProjects/open-source/spring-reading/spring-resources/spring-resource/myfile.txt";
         Resource resource = new FileSystemResource(path);
         try (InputStream is = resource.getInputStream()) {
             // 读取和处理资源内容
